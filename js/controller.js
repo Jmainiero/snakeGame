1b73542efe57d78ae2abd5aec94deb59bdd61815
John <jcmainiero@gmail.com>
2020-03-16 19:40:14 -0400

    directional movement

--- js/controller.js
+++ js/controller.js
@@ -1,14 +1,41 @@
 class controller {
   constructor() {
-    console.log('This is controller');
     this.canvas = document.getElementById('canvas');
     this.ctx = canvas.getContext('2d');
     this.rows = 600;
     this.cols = 600;
+    this.pnt = 20;
     this.newCanvas = new drawing(this.canvas, this.ctx, this.rows, this.cols);
     this.fruit = new fruit(this.cols, this.rows);
-    this.snake = new snake(this.cols, this.rows);
+    this.snake = new snake(0, 0);
+  }
+  moveSnake(evt) {
+    switch (evt.keyCode) {
+      case 37:
+        this.snake.x += -1;
+        this.snake.y += 0;
+        break;
+      case 38:
+        this.snake.x += 0;
+        this.snake.y += -1;
+        break;
+      case 39:
+        this.snake.x += 1;
+        this.snake.y += 0;
+        break;
+      case 40:
+        this.snake.x += 0;
+        this.snake.y += 1;
+        break;
+    }
+    this.snake.buildSnake();
+    console.log(evt.keyCode);
   }
 }
 // const startGame = new controller();
 const startGame = new controller();
+console.log(startGame.snake);
+
+document.addEventListener('keydown', function(evt) {
+  startGame.moveSnake(evt);
+});
--- js/snake.js
+++ js/snake.js
@@ -1,40 +1,20 @@
 class snake {
-  constructor(maxWidth, maxHeight) {
-    this.maxWidth = maxWidth;
-    this.maxHeight = maxHeight;
-    this.x = this.maxWidth / 30;
-    this.y = this.maxHeight / 30;
+  constructor(x, y) {
+    // this.maxWidth = maxWidth;
+    // this.maxHeight = maxHeight;
+    this.x = x;
+    this.y = y;
+    this.nX = -1;
+    this.nY = -1;
     this.snake = [];
     this.buildSnake();
-    this.movement();
   }
   buildSnake() {
     this.ctx = canvas.getContext('2d');
     this.ctx.fillStyle = 'rgb(255, 0, 255)';
-    this.ctx.fillRect(this.x, this.y, 20, 20);
-  }
-  movement() {
-    document.addEventListener('keydown', function(e) {
-      // console.log('moving');
-      switch (e.keyCode) {
-        case 37:
-          this.x = -1;
-          this.y = 0;
-          break;
-        case 38:
-          this.x = 0;
-          this.y = -1;
-          break;
-        case 39:
-          this.x = 1;
-          this.y = 0;
-          break;
-        case 40:
-          this.x = 0;
-          this.y = 1;
-          break;
-      }
-      snake.buildSnake();
-    });
+    this.ctx.fillRect(this.x * 20, this.y * 20, 20, 20);
+    // console.log(
+    //   Math.random(this.maxWidth) + ' this is y  ' + Math.random(this.maxHeight)
+    // );
   }
 }
